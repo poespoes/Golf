577bf83b9330d14ded3d1ad85fe3fb48897b9c8f
Unknown <poe_s@hotmail.com>
2018-04-08 12:29:36 -0400

    Initial commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour {
+    private Rigidbody rb;
+    GameObject club;
+    bool moving;
+    float timer;
+    float winScreen;
+    bool winState;
+
+    // Use this for initialization
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        club = GameObject.Find("Club");
+        rb.velocity = new Vector3(0,0,0);
+    }
+
+    void Update() {
+        if(moving == false) rb.velocity = new Vector3(2,0, 0);
+        if(Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene("menu");
+        timer += Time.deltaTime;
+
+        if(timer >= 7 && !winState) {
+            Debug.Log("RESTART!");
+            SceneManager.LoadScene("main");
+        }
+        if(winState == true) winScreen += Time.deltaTime;
+        if(winScreen >= 5) SceneManager.LoadScene("win");
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("WIN!");
+        winState = true;
+        transform.parent = club.transform;
+        rb.velocity = Vector3.zero;
+        moving = true;
+
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public Vector3 rotateAxis = new Vector3();
+    public float rotateSpeed = 100f;
+    private bool swing;
+    private bool swingBack;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)&& swing == false) {
+            Debug.Log("swing!");
+            swing = true;
+        }
+        if (swing == true) Swing();
+	}
+
+    /*private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Ball") {
+            Debug.Log("WIN!");
+        }
+    }*/
+
+    private void Swing() {
+        if (swing == true && swingBack == false) {
+            transform.Rotate(rotateAxis, rotateSpeed * Time.deltaTime);
+        }
+        //if rotation = n, then swingBack = true
+
+        if (swing == true && swingBack == true) {
+            transform.Rotate(rotateAxis, -rotateSpeed * Time.deltaTime);
+        }
+        /*if (swing == true && swingBack == true && rotation back to original){
+            swingBack = false;
+        }*/
+    }
+}
